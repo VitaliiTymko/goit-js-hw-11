1c625aa3b286a4db9b8412404a36e3d293415225
VitaliiTymko <intehmet2015@gmail.com>
2022-11-19 02:36:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+function onFetch() {
+    fetch('https://pixabay.com/api/?key=31423589-05a77bf58d80d41712d5d29e1&q=women&image_type=photo')
+    .then(r=> r.json())
+    .then(console.log())
+}
+
+onFetch();
